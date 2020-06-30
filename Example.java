f253a905f30bb84736d1fbbc9c059cbda30b7b07
PaulGuilari <appletry85@gmail.com>
2020-06-30 20:25:04 +0300

    initial commit of the project

---
+++ Example.java
@@ -0,0 +1,8 @@
+/*Это простая программа на Java
+  Присвоить классу имя "Example"
+*/
+public class Example {
+    public static void main (String args[]){
+        System.out.println("Простая программа на Java.");
+    }
+}
---
+++ Example2.java
@@ -0,0 +1,16 @@
+/*
+Присвоить классу имя "Example2"
+ */
+
+public class Example2 {
+    public static void main (String args[]){
+        int num; //объявление переменной целочисленного типа
+                //с именем num
+        num = 100; //присвоение значения 100 переменной
+        System.out.println("Это переменная num: " + num);
+        num = num * 2;//рисвоение переменной num нового значения 200
+        System.out.print("Значение переменно num * 2 =  ");
+        System.out.println(num);
+
+    }
+}
